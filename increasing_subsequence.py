90ec5c1ecaf03a99aa2a1260aac475f53aaf851e
hackk <yusheng246@163.com>
2017-10-24 08:28:31 +0800

    modified

--- increasing_subsequence.py
+++ increasing_subsequence.py
@@ -15,10 +15,19 @@ def length_of_longest_increasing_subsequence(array):
             memo[i] = memo[i-1] + 1
         else:
             memo[i] = 1
-    print(memo)
 
     max_length = memo[0]
+    max_index = 0
     for i in range(1,len(array)):
         if memo[i] > max_length:
             max_length = memo[i]
-    return max_length
+            max_index = i
+    print_sub_eles(array, max_index, max_length)
+
+
+def print_sub_eles(array, max_index, max_length):
+    result = []
+    while max_length != 0:
+        result.append(array[max_index])
+        max_index, max_length = max_index-1, max_length-1
+    print(result)
